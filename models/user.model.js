5d4f110f507ed04c41659ebedda016f7834be6d7
kirankumar8658 <kiransingh8658@gmail.com>
2025-07-16 12:19:52 +0530

    Initial commit

---
+++ models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    userId:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        lowercase:true,
+        minlength:[10,"please enter a valid email"],
+        unique:true
+        },
+    userType:{
+        type:String,
+        required:true,
+        default:"CUSTOMER",
+        enum:["CUSTOMER","ADMIN"]
+    }
+});
+
+const User = mongoose.model("User",userSchema);
+module.exports = User;
